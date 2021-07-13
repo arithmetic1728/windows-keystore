425f7ab9dc390cfb5f95778bc34e73e97a55f539
user <user@DESKTOP-F38E1TO>
2021-07-13 01:39:37 -0700

    update

--- my.py
+++ my.py
@@ -6,17 +6,31 @@ from cryptography.hazmat.primitives.serialization import NoEncryption
 import subprocess
 
 
-def get_cert_and_key(password):
+def has_cert_and_key(location, serial_number, password):
     proc = subprocess.Popen(
         [
             'powershell',
             '-c',
-            '&{ $store = New-Object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::My, [System.Security.Cryptography.X509Certificates.StoreLocation]::CurrentUser); ' +
-            '$store.Open("MaxAllowed");' +
-            '$cert = $store.Certificates[0];' +
+            f'&{{ $cert = Get-ChildItem {location} | where {{$_.SerialNumber -eq "{serial_number}" -and $_.HasPrivateKey}}; !($cert -eq $null) }}'
+        ],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE
+    )
+
+    out, err = proc.communicate()
+    if err:
+        raise ValueError(err)
+    return out.rstrip() == b"True"
+
+
+def get_windows_client_cert_source(location, serial_number, password):
+    proc = subprocess.Popen(
+        [
+            'powershell',
+            '-c',
+            f'&{{ $cert = Get-ChildItem {location} | where {{$_.SerialNumber -eq "{serial_number}" -and $_.HasPrivateKey}};' +
             f'$out = $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pfx, {password});' +
-            '[System.Convert]::ToBase64String($out);' +
-            '$store.Close();}'
+            '[System.Convert]::ToBase64String($out);}'
         ],
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE
@@ -26,10 +40,13 @@ def get_cert_and_key(password):
     if err:
         raise ValueError(err)
 
-    #pfx = "MIIFKAIBAzCCBOQGCSqGSIb3DQEHAaCCBNUEggTRMIIEzTCCAjYGCSqGSIb3DQEHAaCCAicEggIjMIICHzCCAhsGCyqGSIb3DQEMCgECoIIBNjCCATIwHAYKKoZIhvcNAQwBAzAOBAgmZ1JfPqf6SwICB9AEggEQZ6GlOr9qCDt+ri1BlS6X1X+WSID92Aep5C9q1ELgT6IIeHgHT/X2y/bBILyIS9kSf8ogIYt2EwDI2c5QNROz/12bhbcziw5JXkV3Je4Rh6n8mojhUT5zIReol9EiII/gU+cCLwX5swIsb0YSD0zdJ0OWbKaWuMQgGK34cq+d42a80TmMQ/4SHpW9b34TzqcKp9fD1SvWTtqEnlgU6HlzJbXCNoJddLYxorhTEMT45FvieDAHqtlzKHs6FZ+VnDzTjqO7zQw5Pt/DRaS9jjAiUgOtp9rMUwwz/G8sU/HET7OJj8m2XGOOmIzSfcLsQfnHb0//F2l6UKNBKMM/o+/LRpvfFaw7T1YmCgT1T4itY7YxgdEwEwYJKoZIhvcNAQkVMQYEBAEAAAAwWwYJKoZIhvcNAQkUMU4eTAB7ADYAMAA1ADQANQBCADEARQAtADUANgA1ADgALQA0ADIANQA3AC0AOQBDADUAQQAtAEQAMAAzAEUAMwA4ADAAOQA5ADEANABFAH0wXQYJKwYBBAGCNxEBMVAeTgBNAGkAYwByAG8AcwBvAGYAdAAgAFMAbwBmAHQAdwBhAHIAZQAgAEsAZQB5ACAAUwB0AG8AcgBhAGcAZQAgAFAAcgBvAHYAaQBkAGUAcjCCAo8GCSqGSIb3DQEHBqCCAoAwggJ8AgEAMIICdQYJKoZIhvcNAQcBMBwGCiqGSIb3DQEMAQMwDgQIVXxvIQw3Jk4CAgfQgIICSP5eAb0qOjyLZ7AkDBKSqWp48v3gh0/rdMFj6LpTd3Iz6fW8pRM/p7WVAeDr+B9uqiCIKoZEdAmA4c1wQlj3VWO9wciYRnOre/LjBuzHjNeKRZEgGGlWo7gNLGjyh0xzEHUEEI0qCJYEJniW137sOEoYRkqmnqLi+AXXqwb++qipTHO87xaUwmYeAAdV64E6O6tLQAF6WvQ+tOhq5J0UrWTP+w1sLFiL6cnLK48Dps6mnzKX8ZaeHujmZL+zYn38HavA3rR09P0kW+r/GRIWQdGFXR9ySsm9Siv6dcoHQ0QqGNzKge6ZmuJfvNy44LeLd0/NfggLYxfd1vEmpA3tEY/4KoTKOLQJ55j4gr45aX4GFqYQ0+KjmgXR3TYkh1KeRJET/qHHvcDJPPceGPxB314VffMYeGmdvy+KnCB5XEFgqE2u960bq1ojMYf14VmOGYrpjAiGP/txNlzjsEjBU54iKRAt8GGqBovfcTWzcLBYYYxVe6E1gE02+ckaSfM2RnqwrAAyIF/+c4GPkyKnh3m+R+xnIwS7T//EfRqfLdDbj2XS2Tac/3W4DEIfWbirrXo/vKy39CenBYQfGNGF/hqkrTlaDr6QNDzqX4B3E8jyaUW0Cj0S4hjnHr7Ure8GTYdl4X2bM5re/ed42/Yiuhxg69TakBk+4JObe8bSERWXTk1zuAHOG/2mQFWGo6zRiZP3yuYvj6mnGTWr4lYiJ5Na0BPDlp9GTFnAiqH9zHfGTwM+wBksWii7RDYyPYh3vsS8ufgS2djLMDswHzAHBgUrDgMCGgQU18N5FCLXrdye7266BMGHR36T6BgEFO1PneLwnowD+2wYyN7ru5sd4WyWAgIH0A=="
-    private_key, certificate, additional_certificates = pkcs12.load_key_and_certificates(base64.b64decode(out[:-2].decode()), password.encode())
+    private_key, certificate, additional_certificates = pkcs12.load_key_and_certificates(base64.b64decode(out.rstrip().decode()), password.encode())
 
-    print(certificate.public_bytes(Encoding.PEM).decode('utf-8'))
-    print(private_key.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption()).decode('utf-8'))
+    def client_cert_source():
+        return certificate.public_bytes(Encoding.PEM), private_key.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption())
 
-get_cert_and_key("12345")
+    return client_cert_source
+
+
+client_cert_source = get_windows_client_cert_source("Cert:\CurrentUser\my", "670DFEA2714C5712E49B96B390D2D4E81B276AB1", "12345")
+print(client_cert_source())
